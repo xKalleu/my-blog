8e6a33065afd2b1a0d69b9182f215850d975f9ff
Kalleu Oliveira <xkalleu@gmail.com>
2020-02-20 22:46:50 -0300

    feat: add page about

---
+++ src/components/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Kalleu Oliveira</h1>
+    <h2>Desenvolvedor Front-End</h2>
+    <p>I'm a front-end developer who loves to work with new challenges and creative projects.
+        At the moment focused and loving studying JavaScript and ReactJS.</p>
+  </div>
+)
+
+export default Profile
