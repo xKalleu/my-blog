ca93db908b82aba757221bd601028c36f563a1da
Kalleu Oliveira <xkalleu@gmail.com>
2020-04-23 21:42:11 -0300

    feat: add images and structure graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
+    position: `Front end Developer`,
     description: `Blog pessoal Kalleu Oliveira`,
     author: `@xkalleu`,
   },
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Profile = () => (
-  <div className="Profile-wrapper">
-    <h1>Kalleu Oliveira</h1>
-    <h2>Desenvolvedor Front-End</h2>
-    <p>I'm a front-end developer who loves to work with new challenges and creative projects.
-        At the moment focused and loving studying JavaScript and ReactJS.</p>
-  </div>
-)
+import Avatar from '../Avatar'
+
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata : { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description
+        }
+      }
+    }    
+  `)
+
+  return (
+    <div className="Profile-wrapper">
+      <Avatar />
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default Profile
