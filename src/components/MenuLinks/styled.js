1b865cbb46ee330f2862986f885580457092836c
Kalleu Oliveira <xkalleu@gmail.com>
2020-05-20 20:54:41 -0300

    feat: add active menu

---
+++ src/components/MenuLinks/styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import variables from '../../styles/variables'
+
+export const MenuLinksWrapper = styled.nav`
+
+`
+
+export const MenuLinksList = styled.ul`
+  font-size: 1.2rem;
+  font-weight: 300;
+`
+
+export const MenuLinksItem = styled.li`
+  padding: 0.5rem 0;
+
+  .active {
+    color: ${variables.quaternaryColor};
+  }
+`
+
+export const MenuLinksLink = styled(Link)`
+  color: ${variables.whiteColor};
+  text-decoration: none;
+  trasition: color 0.5s;
+
+  &:hover {
+    color: ${variables.quaternaryColor};
+  }
+`
