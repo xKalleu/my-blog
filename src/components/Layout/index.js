8a43a950874352e8f2f0b667c0f4a81f156adcc8
Kalleu Oliveira <xkalleu@gmail.com>
2020-05-25 21:06:40 -0300

    feat: add post items home

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Sidebar from "../Sidebar"
+import MenuBar from "../MenuBar"
 
 import * as S from "./styled"
 import GlobalStyles from "../../styles/global"
-import Sidebar from "../Sidebar"
+
 
 const Layout = ({ children }) => {
   return (
@@ -11,6 +13,7 @@ const Layout = ({ children }) => {
       <GlobalStyles />
       <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
     </S.LayoutWrapper>
   )
 }
---
+++ src/components/MenuBar/styled.js
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+import variables from '../../styles/variables'
+
+export const MenuBarWrapper = styled.aside`
+  align-items: center;
+  background: ${variables.primaryColor};
+  border-left: 1px solid #38444d;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: space-between;
+  padding: 0.8rem 0;
+  position: fixed;
+  right: 0;
+  width: 3.75rem;
+`
+
+export const MenuBarGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const MenuBarLink = styled(Link)`
+  display: block;
+`
+
+export const MenuBarItem = styled.span`
+  color:  ${variables.whiteColor};
+  cursor: pointer;
+  display: block;
+  height: 3.75rem;
+  padding: 1.1rem;
+  position: relative;
+  width: 3.75rem;
+  &:hover {
+    color: ${variables.quinaryColor};
+  }
+`
