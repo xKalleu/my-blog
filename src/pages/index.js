773fcbbbf51e819680412a882b4904c8cce5a034
Kalleu Oliveira <kalleu.oliveira@apiki.com>
2020-06-03 23:56:50 -0300

    add markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem
-      slug="/about/"
-      category="Misc"
-      date="30 de Julho de 2019"
-      timeToRead="5"
-      title="Diga não ao Medium: tenha sua própria plataforma"
-      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      } 
+    `
+  )
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <PostItem
+        slug="/about/"
+        category="Misc"
+        date="30 de Julho de 2019"
+        timeToRead="5"
+        title="Diga não ao Medium: tenha sua própria plataforma"
+        description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage
